f43cb3d10d5a96302bc180ad226599a584b3efc8
Neboer <rubinposter@gmail.com>
2020-02-13 21:44:08 +0800

    first commit

---
+++ precache-manifest.a952136c31f229a9ec9d109d8074fad2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "794987e8561635484131fa25a80a7b06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0ffbff905155ff42e19",
+    "url": "/static/css/2.a77db10b.chunk.css"
+  },
+  {
+    "revision": "5df913126c326c4b11e7",
+    "url": "/static/css/main.7fb7e7a5.chunk.css"
+  },
+  {
+    "revision": "a0ffbff905155ff42e19",
+    "url": "/static/js/2.a47ba581.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.a47ba581.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5df913126c326c4b11e7",
+    "url": "/static/js/main.bb187958.chunk.js"
+  },
+  {
+    "revision": "8f0b4df59b29d77be354",
+    "url": "/static/js/runtime-main.3a4875d0.js"
+  }
+]);
